98f0af4a170b8d3f8fb08641207d0d1a688d1e2b
ArturF09 <160763551+ArturF09@users.noreply.github.com>
2024-12-10 21:23:14 +0000

    Update BD

--- BDguru.sql
+++ BDguru.sql
@@ -2,17 +2,19 @@ CREATE DATABASE guru_ip;
 
 USE guru_ip;
 
+CREATE TABLE users (
+    id_users INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(200) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL
+);
+
 CREATE TABLE historico_pesquisa (
     id_pesquisa INT AUTO_INCREMENT PRIMARY KEY,
     ip VARCHAR(45) NOT NULL,
     latitude VARCHAR(15),
     longitude VARCHAR(15),
-    data_pesquisa TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE users(
-	id_users INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(200) NOT NULL,
-    password VARCHAR(100) NOT NULL
+    data_pesquisa TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id_users INT,  
+    FOREIGN KEY (id_users) REFERENCES users(id_users) ON DELETE SET NULL
 );
